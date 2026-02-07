c1b1cb935989355e55919ac736457dcc6c28b35c
Sagar Kharat <153504870+SagarKharat94440@users.noreply.github.com>
2026-02-07 21:37:46 +0530

    Update API and SOCKET URLs to production

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://localhost:3000';
-export const SOCKET_URL = 'http://localhost:3000';
+export const API_BASE_URL = 'https://chat-app-4qbs.onrender.com';
+export const SOCKET_URL = 'https://chat-app-4qbs.onrender.com';
